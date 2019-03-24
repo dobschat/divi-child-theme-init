415ac4c3f1bb0f69ddb42a82ec9e8cd651433a24
Carsten Dobschat <carsten@leo-skull.de>
2019-03-24 14:08:34 +0100

    Update functions.php
    Removes Divi Support Center from Frontend
    
    https://github.com/mirkoschubert/divi-child

--- functions.php
+++ functions.php
@@ -4,4 +4,17 @@
 	    wp_enqueue_style( 'divi', get_template_directory_uri() . '/style.css' );
       wp_enqueue_script( 'divi', get_stylesheet_directory_uri() . 'js/scripts.js', array( 'jquery', 'divi-custom-script' ), '0.1.2', true );
 	}
+
+
+/**
+ * Removes Divi Support Center from Frontend
+ * @since Divi 3.20.1
+ * https://github.com/mirkoschubert/divi-child
+ */
+function divi_child_remove_support_center() {
+	wp_dequeue_script( 'et-support-center' );
+	wp_deregister_script( 'et-support-center' );
+}
+add_action( 'wp_enqueue_scripts', 'divi_child_remove_support_center', 99999 );
+
 ?>
