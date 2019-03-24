cb938ccdbab9f561e2132581829fb2c19078b1d4
Carsten Dobschat <carsten@leo-skull.de>
2019-03-24 14:17:18 +0100

    Update functions.php

--- functions.php
+++ functions.php
@@ -77,3 +77,19 @@ function divi_child_disable_emojis_tinymce( $plugins ) {
     return array();
   }
 }
+
+/**
+* Remove emoji CDN hostname from DNS prefetching hints.
+* https://github.com/mirkoschubert/divi-child
+* @param array $urls URLs to print for resource hints.
+* @param string $relation_type The relation type the URLs are printed for.
+* @return array Difference betwen the two arrays.
+*/
+function divi_child_disable_emojis_remove_dns_prefetch( $urls, $relation_type ) {
+  if ( 'dns-prefetch' == $relation_type ) {
+    $emoji_svg_url = apply_filters( 'emoji_svg_url','https://s.w.org/images/core/emoji/2/svg/' );
+
+    $urls = array_diff( $urls, array( $emoji_svg_url ) );
+  }
+  return $urls;
+}
